52ba1638285b98afee65e677a4f7af6f61e8a6ab
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-11 00:33:22 +0530

    Updated Home component to enhance the "Who is Joseph?" heading with an exclamation mark for improved emphasis.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -129,7 +129,7 @@ function Home() {
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#001220" fill-opacity="1" d="M0,256L40,224C80,192,160,128,240,101.3C320,75,400,85,480,106.7C560,128,640,160,720,154.7C800,149,880,107,960,128C1040,149,1120,235,1200,240C1280,245,1360,171,1400,133.3L1440,96L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path></svg>
 
       <div className='text py-5'>
-        <h3 className='font-bold text-center'>Who is Joseph?</h3>
+        <h3 className='font-bold text-center'>Who is Joseph?!</h3>
         <div className="who-am-i flex-with-center" style={{ backgroundImage: `url('blob-haikei.svg')` }}>
           <div>
             <h3>DevEnthusiast</h3>
