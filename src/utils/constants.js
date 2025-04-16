9e0748f04b3f0227f2218361a6356ddee28049a7
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-04-16 10:44:17 +0530

    Update BASE_URL constants in constants.js to use relative API path; comment out previous URLs for clarity and future reference.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
-// export const BASE_URL = "http://localhost:7777";
+export const BASE_URL = "/api";
 
-export const BASE_URL = "https://myportfolio-server-5fqf.onrender.com";
+// export const BASE_URL = "https://myportfolio-server-5fqf.onrender.com";
+// export const BASE_URL = "http://localhost:7777";
